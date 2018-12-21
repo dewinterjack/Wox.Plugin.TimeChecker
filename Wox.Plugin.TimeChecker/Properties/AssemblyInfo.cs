259b8feff114bc16a57a770bcf1738aeb482f8cd
Jack Dewinter <jackdewinter1@gmail.com>
2018-12-21 17:19:02 +0000

    Initial commit

---
+++ Wox.Plugin.TimeChecker/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Wox.Plugin.TimeChecker")]
+[assembly: AssemblyDescription("A demo wox plugin")]
+[assembly: AssemblyProduct("Wox.Plugin.TimeChecker")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f1b8ceb6-8f0f-4bc7-bd11-27cdef9bc12d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
